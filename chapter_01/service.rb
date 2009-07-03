6481c0d24dac47db0f53d7ae8142997eca03021c
Paul Dix <paul@pauldix.net>
2009-07-03 16:07:38 -0400

    wired up get methods

---
+++ chapter_01/service.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'sinatra'
+require 'dm-core'
+require 'dm-serializer'
+
+DataMapper.setup(:default, {:adapter => "sqlite3", :database => "db.sqlite3"})
+
+class User
+  include DataMapper::Resource
+  include DataMapper::Serialize
+
+  property :name,     String, :key => true
+  property :email,    String
+  property :password, String
+  property :bio,      String
+end
+
+get '/api/v1/users/:name' do
+  user = User.first(params)
+  if user
+    user.to_json(:exclude => :password)
+  else
+    error 404, "user not found"
+  end
+end
--- chapter_01/spec/service_spec.rb
+++ chapter_01/spec/service_spec.rb
@@ -1,28 +1,54 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe "service" do
-  describe "GET on /v1/users/:id" do
-    it "should return a user by id"
-    it "should return a user with a name"
-    it "should return a user with an email"
-    it "should return a user with a password"
-    it "should return a user with a bio"
-    it "should return a 404 for a user that doesn't exist"
+  describe "GET on /api/v1/users/:id" do
+    before(:all) do
+      User.create(:name => "paul", :email => "paul@pauldix.net", :password => "strongpass", :bio => "rubyist")
+    end
+    
+    it "should return a user by name" do
+      get '/api/v1/users/paul'
+      last_response.should be_ok
+      JSON.parse(last_response.body)["name"].should == "paul"
+    end
+    
+    it "should return a user with an email" do
+      get '/api/v1/users/paul'
+      last_response.should be_ok
+      JSON.parse(last_response.body)["email"].should == "paul@pauldix.net"
+    end
+    
+    it "should not return a user's password" do
+      get '/api/v1/users/paul'
+      last_response.should be_ok
+      JSON.parse(last_response.body).should_not have_key("password")
+    end
+    
+    it "should return a user with a bio" do
+      get '/api/v1/users/paul'
+      last_response.should be_ok
+      JSON.parse(last_response.body)["bio"].should == "rubyist"      
+    end
+    
+    it "should return a 404 for a user that doesn't exist" do
+      get '/api/v1/users/foo'
+      last_response.status.should == 404
+    end
   end
   
-  describe "POST on /v1/users" do
+  describe "POST on /api/v1/users" do
     it "should create a user"    
   end
   
-  describe "PUT on /v1/users/:id" do
+  describe "PUT on /api/v1/users/:id" do
     it "should update a user"    
   end
   
-  describe "DELETE on /v1/users/:id" do
+  describe "DELETE on /api/v1/users/:id" do
     it "should delete a user"    
   end
   
-  describe "POST on /v1/users/:id/sessions" do
+  describe "POST on /api/v1/users/:id/sessions" do
     it "should verify login credentials"
     it "should return the full user object"
   end
