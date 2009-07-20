74f0bd317a63ce627ac32721ff6b98e73b9a78b0
Paul Dix <paul@pauldix.net>
2009-07-20 08:46:01 -0400

    removed the define_remote_method calls and opted for a more clear use of regular get, put, post, and delete inside of written methods.

--- chapter_01/client.rb
+++ chapter_01/client.rb
@@ -4,49 +4,45 @@ require 'json'
 
 class User
   include Typhoeus
-  remote_defaults :base_uri => "http://localhost:3000"
-  define_remote_method :find, 
-    :path => "/api/v1/users/:name",
-    :on_success => lambda {|res| JSON.parse(res.body)},
-    :on_failure => lambda {|response| 
-      if response.code == 404
-        nil
-      else
-        raise response.body
-      end}
-  define_remote_method :create_remote,
-    :method => :post,
-    :path => "/api/v1/users",
-    :on_success => lambda {|res| JSON.parse(res.body)}
-  define_remote_method :update_remote,
-    :method => :put,
-    :path => "/api/v1/users/:name",
-    :on_success => lambda {|res| JSON.parse(res.body)}
-  define_remote_method :destroy,
-    :method => :delete,
-    :path => "/api/v1/users/:name",
-    :on_success => lambda {|res| true}
-  define_remote_method :verify,
-    :method => :post,
-    :path => "/api/v1/users/:name/sessions",
-    :on_success => lambda {|res| JSON.parse(res.body)},
-    :on_failure => lambda {|response|
-      if response.code == 400
-        nil
-      else
-        raise response.body
-      end}
-                       
+  class << self; attr_accessor :base_uri end
+  
+  def self.find_by_name(name)
+    get("#{base_uri}/api/v1/users/#{name}", 
+      :on_success => lambda {|res| JSON.parse(res.body)},
+      :on_failure => lambda {|response| 
+        if response.code == 404
+          nil
+        else
+          raise response.body
+        end})
+  end
+    
   def self.create(attributes = {})
-    create_remote(:body => attributes.to_json)
+    post("#{base_uri}/api/v1/users",
+      :body       => attributes.to_json,
+      :on_success => lambda {|res| JSON.parse(res.body)})
   end
   
-  def self.update(name, attrs)
-    update_remote(:name => name, :body => attrs.to_json)
+  def self.update(name, attributes)
+    put("#{base_uri}/api/v1/users/#{name}",
+      :body       => attributes.to_json,
+      :on_success => lambda {|res| JSON.parse(res.body)})
   end
   
-  def self.login(username, password)
-    verify(:name => username, 
-           :body => {:password => password}.to_json)
+  def self.destroy(name)
+    delete("#{base_uri}/api/v1/users/#{name}",
+      :on_success => lambda {|response| true})
+  end
+  
+  def self.login(name, password)
+    post("#{base_uri}/api/v1/users/#{name}/sessions",
+      :body       => {:password => password}.to_json,
+      :on_success => lambda {|res| JSON.parse(res.body)},
+      :on_failure => lambda {|response|
+        if response.code == 400
+          nil
+        else
+          raise response.body
+        end})
   end
 end
--- chapter_01/spec/client_spec.rb
+++ chapter_01/spec/client_spec.rb
@@ -7,6 +7,7 @@ require File.dirname(__FILE__) + '/../client'
 # the database. We could change this by deleting all users in the test setup.
 describe "client" do
   before(:all) do
+    User.base_uri = "http://localhost:3000"
     User.create(
       :name => "paul", 
       :email => "paul@pauldix.net", 
@@ -20,14 +21,14 @@ describe "client" do
   end
   
   it "should get a user" do
-    user = User.find(:name => "paul")
+    user = User.find_by_name("paul")
     user["name"].should  == "paul"
     user["email"].should == "paul@pauldix.net"
     user["bio"].should   == "rubyist"
   end
   
   it "should return nil for a user not found" do
-    User.find(:name => "gosling").should be_nil
+    User.find_by_name("gosling").should be_nil
   end
   
   it "should create a user" do
@@ -37,19 +38,19 @@ describe "client" do
       :password => "whatev"})
     user["name"].should  == "trotter"
     user["email"].should == "no spam"
-    User.find(:name => "trotter").should == user
+    User.find_by_name("trotter").should == user
   end
   
   it "should update a user" do
-    user = User.update("paul", {:bio => "rubyist and author"})
+    user = User.update("paul", :bio => "rubyist and author")
     user["name"].should == "paul"
     user["bio"].should  == "rubyist and author"
-    User.find(:name => "paul").should == user
+    User.find_by_name("paul").should == user
   end
   
   it "should destroy a user" do
-    User.destroy(:name => "bryan").should == true
-    User.find(:name => "bryan").should be_nil
+    User.destroy("bryan").should == true
+    User.find_by_name("bryan").should be_nil
   end
   
   it "should verify login credentials" do
