c03a4a4c211bede0b214f1d11f2dd5fa32f12de7
Paul Dix <paul@pauldix.net>
2010-12-05 15:58:05 -0500

    fixed the client specs to work with newer version of typhoeus

--- chapter_01/client.rb
+++ chapter_01/client.rb
@@ -1,48 +1,53 @@
 require 'rubygems'
+require 'bundler/setup'
 require 'typhoeus'
 require 'json'
 
 class User
-  include Typhoeus
   class << self; attr_accessor :base_uri end
-  
+
   def self.find_by_name(name)
-    get("#{base_uri}/api/v1/users/#{name}", 
-      :on_success => lambda {|res| JSON.parse(res.body)},
-      :on_failure => lambda {|response| 
-        if response.code == 404
-          nil
-        else
-          raise response.body
-        end})
+    response = Typhoeus::Request.get("#{base_uri}/api/v1/users/#{name}")
+    if response.code == 200
+      JSON.parse(response.body)["user"]
+    elsif response.code == 404
+      nil
+    else
+      raise response.body
+    end
   end
-    
+
   def self.create(attributes = {})
-    post("#{base_uri}/api/v1/users",
-      :body       => attributes.to_json,
-      :on_success => lambda {|res| JSON.parse(res.body)})
+    response = Typhoeus::Request.post("#{base_uri}/api/v1/users", :body => attributes.to_json)
+    if response.success?
+      JSON.parse(response.body)["user"]
+    else
+      raise response.body
+    end
   end
-  
+
   def self.update(name, attributes)
-    put("#{base_uri}/api/v1/users/#{name}",
-      :body       => attributes.to_json,
-      :on_success => lambda {|res| JSON.parse(res.body)})
+    response = Typhoeus::Request.put("#{base_uri}/api/v1/users/#{name}", :body => attributes.to_json)
+    if response.success?
+      JSON.parse(response.body)["user"]
+    else
+      raise response.body
+    end
   end
-  
+
   def self.destroy(name)
-    delete("#{base_uri}/api/v1/users/#{name}",
-      :on_success => lambda {|response| true})
+    response = Typhoeus::Request.delete("#{base_uri}/api/v1/users/#{name}")
+    response.success?
   end
-  
+
   def self.login(name, password)
-    post("#{base_uri}/api/v1/users/#{name}/sessions",
-      :body       => {:password => password}.to_json,
-      :on_success => lambda {|res| JSON.parse(res.body)},
-      :on_failure => lambda {|response|
-        if response.code == 400
-          nil
-        else
-          raise response.body
-        end})
+    response = Typhoeus::Request.post("#{base_uri}/api/v1/users/#{name}/sessions", :body => {:password => password}.to_json)
+    if response.success?
+      JSON.parse(response.body)["user"]
+    elsif response.code == 400
+      nil
+    else
+      raise response.body
+    end
   end
 end
--- chapter_01/spec/client_spec.rb
+++ chapter_01/spec/client_spec.rb
@@ -8,56 +8,58 @@ require File.dirname(__FILE__) + '/../client'
 describe "client" do
   before(:all) do
     User.base_uri = "http://localhost:3000"
+
+    User.destroy("paul")
+    User.destroy("trotter")
+
     User.create(
-      :name => "paul", 
-      :email => "paul@pauldix.net", 
-      :password => "strongpass", 
+      :name => "paul",
+      :email => "paul@pauldix.net",
+      :password => "strongpass",
       :bio => "rubyist")
     User.create(
-      :name => "bryan", 
-      :email => "no spam", 
-      :password => "strongpass", 
+      :name => "bryan",
+      :email => "bryan@spamtown.usa",
+      :password => "strongpass",
       :bio => "rubyist")
   end
-  
+
   it "should get a user" do
     user = User.find_by_name("paul")
     user["name"].should  == "paul"
     user["email"].should == "paul@pauldix.net"
     user["bio"].should   == "rubyist"
   end
-  
+
   it "should return nil for a user not found" do
     User.find_by_name("gosling").should be_nil
   end
-  
+
   it "should create a user" do
     user = User.create({
-      :name => "trotter", 
-      :email => "no spam", 
+      :name => "trotter",
+      :email => "trotter@spamtown.usa",
       :password => "whatev"})
-    user["name"].should  == "trotter"
-    user["email"].should == "no spam"
-    User.find_by_name("trotter").should == user
+    User.find_by_name("trotter")["email"].should == "trotter@spamtown.usa"
   end
-  
+
   it "should update a user" do
     user = User.update("paul", :bio => "rubyist and author")
     user["name"].should == "paul"
     user["bio"].should  == "rubyist and author"
-    User.find_by_name("paul").should == user
+    User.find_by_name("paul")["bio"] == "rubyist and author"
   end
-  
+
   it "should destroy a user" do
     User.destroy("bryan").should == true
     User.find_by_name("bryan").should be_nil
   end
-  
+
   it "should verify login credentials" do
     user = User.login("paul", "strongpass")
     user["name"].should == "paul"
   end
-  
+
   it "should return nil with invalid credentials" do
     User.login("paul", "wrongpassword").should be_nil
   end
